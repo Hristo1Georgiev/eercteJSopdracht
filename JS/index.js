a2795f4ec0e50d3154bb67e8cde9257405d17348
Hristo Georgiev <83413781+Hristo1Georgiev@users.noreply.github.com>
2021-06-23 10:54:37 +0200

    Add files via upload
    eerste java script opdracht.

---
+++ JS/index.js
@@ -0,0 +1,14 @@
+let name = 'Chris';
+console.log(name);
+console.log("Hello Winc Academy");
+//coment
+console.log(4 + 4)
+console.log(8 - 4)
+console.log(8 * 4)
+console.log(15 / 3)
+console.log(30 / 100)
+let x = 5;
+let y = x % 2;
+console.log(y)
+let age = 'forty nine';
+console.log(age)
